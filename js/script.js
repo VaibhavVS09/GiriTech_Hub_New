d4d5fdb400ae0a9009ffa9726d0a5452308c5093
Vaibhav Satvekar <vaibhavsatvrkar09@gmail.com>
2024-10-02 08:31:51 +0530

    Added Section
    Added Navbar. Herobanner and counter section to code base

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function incrementStats() {
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = counter.getAttribute("data-target");
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 10);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCounter();
+  });
+}
+document.addEventListener("DOMContentLoaded", incrementStats);
