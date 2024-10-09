031519e715a220ae1f5b5955cf68a88294f6327f
Vaibhav Satvekar <vaibhavsatvrkar09@gmail.com>
2024-10-09 09:40:18 +0530

    Added  The Course Page
    Added new page of course with herobanner

--- js/script.js
+++ js/script.js
@@ -21,3 +21,27 @@ function incrementStats() {
   });
 }
 document.addEventListener("DOMContentLoaded", incrementStats);
+
+//Video Modal
+const videoBtn = document.querySelector(".video-btn");
+const videoModal = document.querySelector("#videoModal");
+const video = document.querySelector("#video");
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener("click", () => {
+    videoSrc = videoBtn.getAttribute("data-bs-src");
+  });
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener("shown.bs.modal", () => {
+    video.setAttribute(
+      "src",
+      videoSrc + "?autoplay=1;modestbranding=1;showinfo=0"
+    );
+  });
+  videoModal.addEventListener("hide.bs.modal", () => {
+    video.setAttribute("src", videoSrc);
+  });
+}
